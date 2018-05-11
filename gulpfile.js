c0c0fa160077079e586fb5957ad06220b850437b
Anders Urban <minikaliffen@gmail.com>
2018-05-11 17:09:05 +0200

    Updated gitignoe and extension v0.0.2 - fixes shopping cart images

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+let gulp = require("gulp");
+let  minifycss = require('gulp-clean-css');
+let  zip = require('gulp-zip');
+let del = require("del");
+
+gulp.task('css', ()=>{
+    return gulp.src("*.css")
+    .pipe(minifycss())
+    .pipe(gulp.dest("build/src"))
+})
+gulp.task("manifest",()=>{
+    return gulp.src("manifest.json")
+    .pipe(gulp.dest("build/src"))
+})
+
+gulp.task("zip",()=>{
+    return gulp.src("build/src/*")
+    .pipe(zip("bilkatogo.zip"))
+    .pipe(gulp.dest("build"))
+})
+
+gulp.task("clean",()=>{
+return del([
+    "build/**/*"
+])
+});
+
+gulp.task("default", gulp.series("clean","css","manifest","zip",(done)=>{
+    done();
+}));
+
