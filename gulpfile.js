89ccd1470041428a3b2967b25dc92a0b4f683632
Anders Urban <minikaliffen@gmail.com>
2018-05-11 17:25:14 +0200

    Cleanup of gulp file and formating

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,57 @@
 let gulp = require("gulp");
-let  minifycss = require('gulp-clean-css');
-let  zip = require('gulp-zip');
+let minifycss = require('gulp-clean-css');
+let zip = require('gulp-zip');
 let del = require("del");
 
-gulp.task('css', ()=>{
-    return gulp.src("*.css")
-    .pipe(minifycss())
-    .pipe(gulp.dest("build/src"))
+/************************
+ *                  	*
+ *	mininfy and moving	*
+ *                  	*
+************************/
+gulp.task('css', () => {
+	return gulp.src("*.css")
+		.pipe(minifycss())
+		.pipe(gulp.dest("build/src"))
 })
-gulp.task("manifest",()=>{
-    return gulp.src("manifest.json")
-    .pipe(gulp.dest("build/src"))
+gulp.task("manifest", () => {
+	return gulp.src("manifest.json")
+		.pipe(gulp.dest("build/src"))
 })
 
-gulp.task("zip",()=>{
-    return gulp.src("build/src/*")
-    .pipe(zip("bilkatogo.zip"))
-    .pipe(gulp.dest("build"))
+/************************
+ *                  	*
+ *      zipping     	*
+ *                  	*
+************************/
+gulp.task("zip", () => {
+	return gulp.src("build/src/*")
+		.pipe(zip("bilkatogo.zip"))
+		.pipe(gulp.dest("build"))
 })
 
-gulp.task("clean",()=>{
-return del([
-    "build/**/*"
-])
+/************************
+ *						*
+ *      Cleaning		*
+ *                 	 	*
+************************/
+
+gulp.task("clean", () => {
+	return del([
+		"build/**/*"
+	])
 });
 
-gulp.task("build",gulp.parallel("css","manifest"),(done)=>{
-    done();
+/************************
+ *                  	*
+ *      Build task  	*
+ *                  	*
+************************/
+
+gulp.task("build", gulp.parallel("css", "manifest"), (done) => {
+	done();
 })
 
-gulp.task("default", gulp.series("clean","build","zip",(done)=>{
-    done();
+gulp.task("default", gulp.series("clean", "build", "zip", (done) => {
+	done();
 }));
 
